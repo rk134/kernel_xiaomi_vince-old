9c81db335545a568c26f35911207246d5df25caf
Lau <laststandrighthere@gmail.com>
2020-09-01 10:37:37 +0800

    techpack: tas2557: Use power efficient wq
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- techpack/audio/asoc/codecs/tas2557-regmap.c
+++ techpack/audio/asoc/codecs/tas2557-regmap.c
@@ -344,10 +344,12 @@ void tas2557_enableIRQ(struct tas2557_priv *pTAS2557, bool enable)
 		if (!pTAS2557->mbIRQEnable) {
 			if (gpio_is_valid(pTAS2557->mnGpioINT)) {
 				enable_irq(pTAS2557->mnIRQ);
-				/* check after 10 ms */
-				queue_delayed_work(system_power_efficient_wq,
-						   &pTAS2557->irq_work,
-						   msecs_to_jiffies(10));
+				if (startup_chk) {
+					/* check after 10 ms */
+					queue_delayed_work(system_power_efficient_wq,
+							   &pTAS2557->irq_work,
+							   msecs_to_jiffies(10));
+				}
 				pTAS2557->mbIRQEnable = true;
 			}
 		}

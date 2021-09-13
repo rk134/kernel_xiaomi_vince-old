288385ec934ec34f3966843c5dc3f78af0d3a365
dlwlrma123 <alexfinhart@gmail.com>
2021-09-13 09:15:29 +0800

    kernel/sched: uclamp:  Mitigate non-boosted tasks preempting boosted tasks
    * by default uclamp_boosted doesn't do rcu locking like schedtune_task_boost, basically its like schedtune_task_boost_rcu_locked. With this we might get the same benefits from [1]
    
    [1]:https://github.com/kdrag0n/proton_zf6/commit/567ea3c45e9c42eaa86e56bda9137a8431f66880#
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7361,6 +7361,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	int isolated_candidate = -1;
 	int prev_cpu = task_cpu(p);
 	bool next_group_higher_cap = false;
+	struct task_struct *curr_tsk;
 
 	*backup_cpu = -1;
 
@@ -7750,6 +7751,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	 *   a) ACTIVE CPU: target_cpu
 	 *   b) IDLE CPU: best_idle_cpu
 	 */
+	 	if (target_cpu != -1 && !idle_cpu(target_cpu) &&
+			best_idle_cpu != -1) {
+		curr_tsk = READ_ONCE(cpu_rq(target_cpu)->curr);
+		if (curr_tsk && uclamp_boosted(curr_tsk)) {
+			target_cpu = best_idle_cpu;
+		}
+	}
 
 	if (prefer_idle && (best_idle_cpu != -1))
 		return best_idle_cpu;
